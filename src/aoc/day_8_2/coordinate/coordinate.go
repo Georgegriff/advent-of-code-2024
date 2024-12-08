cbd86d6ba6f8890a52261700a88483dc2c5417f5
George Griffiths <george.griffiths@immersivelabs.com>
2024-12-08 23:17:29 +0000

    removed additional check

--- src/aoc/day_8_2/coordinate/coordinate.go
+++ src/aoc/day_8_2/coordinate/coordinate.go
@@ -65,9 +65,7 @@ func (coord *Coordinate) CalculateAntinodePoints(other *Coordinate, yMax int, xM
 	for i := -coord.X; i <= xMax-coord.X; i++ {
 		x := coord.X + distance.X*i
 		y := coord.Y + distance.Y*i
-		if x >= 0 && x <= xMax && y >= 0 && y <= yMax {
-			points = append(points, Coordinate{X: x, Y: y})
-		}
+		points = append(points, Coordinate{X: x, Y: y})
 	}
 	return points
 }
