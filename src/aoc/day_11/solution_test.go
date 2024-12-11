b670e19fbcd85dfd850d2d9f103c607b6bfba1f7
George Griffiths <george.griffiths@immersivelabs.com>
2024-12-11 12:17:37 +0000

    added test for input

--- src/aoc/day_11/solution_test.go
+++ src/aoc/day_11/solution_test.go
@@ -9,3 +9,8 @@ func TestSolve(t *testing.T) {
 	solution := Solve("./test.txt", 25)
 	testutils.ExpectToMatchInt(t, solution, 55312)
 }
+
+func TestSolveInput(t *testing.T) {
+	solution := Solve("./input.txt", 75)
+	testutils.ExpectToMatchInt(t, solution, 221280540398419)
+}
